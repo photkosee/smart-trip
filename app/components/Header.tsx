91c39ac232f172929f73ab3bdb9c55a43d82aedb
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-09-05 02:39:09 +1000

    add: logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
@@ -22,12 +23,21 @@ const Header = () => {
       className={`
       ${
         top ? "bg-white shadow-lg dark:bg-accent" : "dark:bg-transparent"
-      } sticky py-4 top-0 z-30 transition-all w-full flex
+      } sticky py-3 top-0 z-30 transition-all w-full flex
       `}
     >
       <div className="container max-w-6xl mx-auto px-5">
         <div className="flex justify-between items-center gap-x-6 mx-auto">
-          <h2 className="text-xl">Smart Trip</h2>
+          <div className="flex gap-x-1 items-center">
+            <Image
+              src="/smart-trip-logo.png"
+              alt="Logo"
+              priority
+              width={35}
+              height={35}
+            />
+            <h2 className="text-xl">Smart Trip</h2>
+          </div>
           <Button className="self-center rounded-full">Sign In</Button>
         </div>
       </div>
