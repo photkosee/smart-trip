af6762af9c691eac236fa07249336f2a31f14d79
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-09-06 01:35:16 +1000

    add: a route returning generated plan (connect with gemini)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# <p align="center"><a href=""><img width="350" src="./public/logo.png"></a></p>
+# <p align="center"><a href=""><img width="450" src="./public/logo.png"></a></p>
 
 <p align="center"> Create, customize, and optimize your itineraries with this free AI trip planner. <a href="">Currently under development</a></p>
 
---
+++ app/api/generate-trip/route.ts
@@ -0,0 +1,58 @@
+import { NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+export async function GET(req: Request) {
+  try {
+    const data = await req.json();
+    const { place, dayCount, companions, peopleCount, budget } = data;
+
+    const prompt = process.env.NEXT_PUBLIC_AI_PROMPT!
+      .replace("$PLACE", place)
+      .replace("$DAYCOUNT", dayCount.toString())
+      .replace("$COMPANIONS", companions)
+      .replace("$PEOPLECOUNT", peopleCount.toString())
+      .replace("$BUDGET", budget.toString());
+
+    const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_AI_API!);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+    });
+
+    const generationConfig = {
+      temperature: 1,
+      topP: 0.95,
+      topK: 64,
+      maxOutputTokens: 8192,
+      responseMimeType: "application/json",
+    };
+
+    const chatSession = model.startChat({
+      generationConfig,
+      // safetySettings: Adjust safety settings
+      // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+        {
+          role: "user",
+          parts: [
+            {text: process.env.NEXT_PUBLIC_AI_SAMPLE_TEXT!},
+          ],
+        },
+        {
+          role: "model",
+          parts: [
+            {text: process.env.NEXT_PUBLIC_AI_SAMPLE_OUTPUT!},
+          ],
+        },
+      ],
+    });
+
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+    const output = response.text();
+
+    return NextResponse.json({ output: output }, { status: 200 });
+  } catch (error) {
+    console.error("Error occurred:", error);
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+};
