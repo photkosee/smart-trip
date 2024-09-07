066484f369d3dfb89193f65247750c5413de0f08
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-09-08 02:24:55 +1000

    fix: hydration (content mismatch on client vs server side)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
+import dynamic from "next/dynamic";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import "@/app/globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import Header from "@/app/components/Header";
-import Footer from "@/app/components/Footer";
 import StoreProvider from "@/app/StoreProvider";
+
+const Header = dynamic(() => import("@/app/components/Header"), {
+  ssr: false, // Disable SSR for this component
+});
+const Footer = dynamic(() => import("@/app/components/Footer"), {
+  ssr: false,
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +20,7 @@ export const metadata: Metadata = {
   title: "Smart Trip",
   description:
     "Create, customize, and optimize your itineraries with this free AI trip planner. Perfect for vacations, workations, and everyday adventures.",
+  authors: [{ name: "Phot Koseekrainiramon" }],
 };
 
 export default function RootLayout({
